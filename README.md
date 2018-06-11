04656bee3448b8a14c2961b7e9a0afb77352e585
pr0crustes <dark.pr0crustes@gmail.com>
2018-06-11 12:24:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pyskal
+
+Pyskal is a command line python3 script that receives a text input and converts it into a image.
+
+Pyskal dependencies are:
+    numpy
+    matplotlib
+    scipy
+
+ 
---
+++ plot_string.py
@@ -0,0 +1,80 @@
+import hashlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.misc import imresize
+
+
+def hash_string(text):
+    hashed1 = hashlib.md5(text.encode("utf-8")).hexdigest()
+    hashed2 = hashlib.md5(hashed1.encode("utf-8")).hexdigest()
+
+    return hashed2  # Always len = 32
+
+
+def hexstring_rest(text):
+
+    values = []
+
+    for letter in text:
+
+        as_int = int(letter, 16)
+
+        is_odd = (as_int & 1)  # 1 if odd
+
+        values.append(is_odd)
+
+    return values
+
+
+def get_array_image(text):
+
+    hashed = hash_string(text)
+
+    pad = [0] * 2
+
+    array = np.array(pad + hexstring_rest(hashed) + pad).reshape(6, 6)
+
+    resized_array = imresize(array, 25 * 100, interp='nearest')
+
+    return resized_array
+
+
+def apply_symmetry(array):
+
+    flip_up = np.flip(array, 0)
+    flip_lr = np.flip(array, 1)
+
+    flip_both = np.flip(flip_up, 1)
+
+    upper = np.concatenate([flip_up, flip_both], 1)
+
+    bottom = np.concatenate([array, flip_lr], 1)
+
+    full = np.concatenate([upper, bottom], 0)
+
+    return full
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("string")
+    args = parser.parse_args()
+
+    if not args.string:
+        print("Correct use:\n$  python3 plot_string.py STRING_OF_CHOICE")
+        exit()
+
+    user_input = args.string
+
+    print("Running with input: `{}`".format(user_input))
+
+    array = get_array_image(user_input)
+    array = apply_symmetry(array)
+
+    plt.imshow(array, cmap="gray")
+    plt.show()
